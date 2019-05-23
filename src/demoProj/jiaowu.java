c200a975f2063400ea4a53cebeab20e5bb584839
XS170202105957 <2229826752@qq.com>
2019-05-23 20:02:56 +0800

    second

--- src/demoProj/jiaowu.java
+++ src/demoProj/jiaowu.java
@@ -2,7 +2,8 @@ package demoProj;
 
 public class jiaowu {
 	public static void main(String[] args) {
-		System.out.println("good!");
+		//System.out.println("good!");
+		System.out.println("good branch!");
 	}
 
 
